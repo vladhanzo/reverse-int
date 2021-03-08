a05ba131130caf2d550e09fb411e8ff897553c09
Владислав Капустин <70846638+vladhanzo@users.noreply.github.com>
2021-03-08 13:53:16 +0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = n.toString();
+    let x = n.split("").reverse().join("");
+    return parseInt(x);
+};
